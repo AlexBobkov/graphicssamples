00a1bdf34ee44222cdc0ab018423d61adc5401c2
Alexander Bobkov <alexander.e.bobkov@gmail.com>
2016-02-16 00:36:48 +0300

    Added -std=c++11 option

--- cmake/MacroUtils.cmake
+++ cmake/MacroUtils.cmake
@@ -4,6 +4,10 @@ macro(MAKE_SAMPLE TRGTNAME)
 	target_include_directories(${TRGTNAME} PUBLIC ${PROJECT_SOURCE_DIR}/common)
 
 	target_link_libraries(${TRGTNAME} GLEW::glew_s glm glfw ASSIMP::assimp SOIL::soil ${ANTTWEAKBAR_LIBRARY})
+	
+	if (CMAKE_COMPILER_IS_GNUCC)
+		target_compile_options(${TRGTNAME} PUBLIC -std=c++11)
+	endif ()
 
 	install(TARGETS ${TRGTNAME} RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX})
 endmacro(MAKE_SAMPLE TRGTNAME)
@@ -14,6 +18,10 @@ macro(MAKE_TASK TRGTNAME)
 	target_include_directories(${TRGTNAME} PUBLIC ${PROJECT_SOURCE_DIR}/common)
 
 	target_link_libraries(${TRGTNAME} GLEW::glew_s glm glfw ASSIMP::assimp SOIL::soil ${ANTTWEAKBAR_LIBRARY})
+	
+	if (CMAKE_COMPILER_IS_GNUCC)
+		target_compile_options(${TRGTNAME} PUBLIC -std=c++11)
+	endif ()
 
 	install(TARGETS ${TRGTNAME} RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX})
 endmacro(MAKE_TASK TRGTNAME)
