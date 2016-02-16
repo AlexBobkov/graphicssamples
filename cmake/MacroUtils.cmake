6e35f6d89f38740191c34f0086ce12397571ed5f
Alexander Bobkov <alexander.e.bobkov@gmail.com>
2016-02-17 00:53:06 +0300

    Removed students folder

--- cmake/MacroUtils.cmake
+++ cmake/MacroUtils.cmake
@@ -11,17 +11,3 @@ macro(MAKE_SAMPLE TRGTNAME)
 
 	install(TARGETS ${TRGTNAME} RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX})
 endmacro(MAKE_SAMPLE TRGTNAME)
-
-macro(MAKE_TASK TRGTNAME)
-	add_executable(${TRGTNAME} ${SRC_FILES} ${HEADER_FILES})
-	
-	target_include_directories(${TRGTNAME} PUBLIC ${PROJECT_SOURCE_DIR}/common)
-
-	target_link_libraries(${TRGTNAME} GLEW::glew_s glm glfw ASSIMP::assimp SOIL::soil imgui)
-	
-	if (CMAKE_COMPILER_IS_GNUCC)
-		target_compile_options(${TRGTNAME} PUBLIC -std=c++11)
-	endif ()
-
-	install(TARGETS ${TRGTNAME} RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX})
-endmacro(MAKE_TASK TRGTNAME)
