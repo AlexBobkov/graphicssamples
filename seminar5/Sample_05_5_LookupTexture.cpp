27d9142aceaf79da794f9a38c48e9268a7cbe5ed
Alexander Bobkov <alexander.e.bobkov@gmail.com>
2016-03-17 00:09:40 +0300

    Added texture lookup example

---
+++ seminar5/Sample_05_5_LookupTexture.cpp
@@ -0,0 +1,219 @@
+#include <Application.hpp>
+#include <Mesh.hpp>
+#include <ShaderProgram.hpp>
+#include <Texture.hpp>
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+struct LightInfo
+{
+    glm::vec3 position; //Будем здесь хранить координаты в мировой системе координат, а при копировании в юниформ-переменную конвертировать в систему виртуальной камеры
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+};
+
+/**
+Пример с текстурированием разных 3д-моделей
+*/
+class SampleApplication : public Application
+{
+public:
+    MeshPtr _cube;
+    MeshPtr _sphere;
+    MeshPtr _bunny;
+
+    MeshPtr _marker; //Меш - маркер для источника света
+
+    //Идентификатор шейдерной программы
+    ShaderProgramPtr _shader;
+    ShaderProgramPtr _markerShader;
+
+    //Переменные для управления положением одного источника света
+    float _lr;
+    float _phi;
+    float _theta;
+
+    LightInfo _light;
+
+    TexturePtr _worldTexture;
+    TexturePtr _lookupTexture;
+
+    GLuint _sampler;
+
+    void makeScene() override
+    {
+        Application::makeScene();
+
+        //=========================================================
+        //Создание и загрузка мешей		
+
+        _cube = makeCube(0.5f);
+        _cube->setModelMatrix(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -1.0f, 0.5f)));
+
+        _sphere = makeSphere(0.5f);
+        _sphere->setModelMatrix(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.5f)));
+
+        _bunny = loadFromFile("models/bunny.obj");
+        _bunny->setModelMatrix(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 1.0f, 0.0f)));
+
+        _marker = makeSphere(0.1f);
+
+        //=========================================================
+        //Инициализация шейдеров
+
+        _shader = std::make_shared<ShaderProgram>();
+        _shader->createProgram("shaders5/textureLookup.vert", "shaders5/textureLookup.frag");
+
+        _markerShader = std::make_shared<ShaderProgram>();
+        _markerShader->createProgram("shaders/marker.vert", "shaders/marker.frag");
+
+        //=========================================================
+        //Инициализация значений переменных освщения
+        _lr = 3.0;
+        _phi = 0.0;
+        _theta = glm::pi<float>() * 0.25f;
+
+        _light.position = glm::vec3(glm::cos(_phi) * glm::cos(_theta), glm::sin(_phi) * glm::cos(_theta), glm::sin(_theta)) * (float)_lr;
+        _light.ambient = glm::vec3(0.2, 0.2, 0.2);
+        _light.diffuse = glm::vec3(0.8, 0.8, 0.8);
+        _light.specular = glm::vec3(1.0, 1.0, 1.0);
+
+        //=========================================================
+        //Загрузка и создание текстур
+        _worldTexture = loadTexture("images/earth_global.jpg");
+
+        //Создаем lookup-текстуру
+        int width = 128;
+        int height = 128;
+
+        std::vector<GLubyte> data;
+
+        for (int row = 0; row < height; row++)
+        {
+            for (int column = 0; column < width; column++)
+            {   
+                float dot = 1.0f * column / (width - 1);
+                float shininess = 255.0f * row / (height - 1);
+
+                data.push_back(static_cast<GLubyte>(255 * pow(dot, shininess)));
+            }
+        }
+
+        _lookupTexture = std::make_shared<Texture>(GL_TEXTURE_2D);
+        _lookupTexture->setTexImage2D(GL_TEXTURE_2D, 0, GL_R8, width, height, GL_RED, GL_UNSIGNED_BYTE, data.data());
+
+        //=========================================================
+        //Инициализация сэмплера, объекта, который хранит параметры чтения из текстуры
+        glGenSamplers(1, &_sampler);
+        glSamplerParameteri(_sampler, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glSamplerParameteri(_sampler, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glSamplerParameteri(_sampler, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glSamplerParameteri(_sampler, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    }
+
+    void updateGUI() override
+    {
+        Application::updateGUI();
+
+        ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiSetCond_FirstUseEver);
+        if (ImGui::Begin("MIPT OpenGL Sample", NULL, ImGuiWindowFlags_AlwaysAutoResize))
+        {
+            ImGui::Text("FPS %.1f", ImGui::GetIO().Framerate);
+
+            if (ImGui::CollapsingHeader("Light"))
+            {
+                ImGui::ColorEdit3("ambient", glm::value_ptr(_light.ambient));
+                ImGui::ColorEdit3("diffuse", glm::value_ptr(_light.diffuse));
+                ImGui::ColorEdit3("specular", glm::value_ptr(_light.specular));
+
+                ImGui::SliderFloat("radius", &_lr, 0.1f, 10.0f);
+                ImGui::SliderFloat("phi", &_phi, 0.0f, 2.0f * glm::pi<float>());
+                ImGui::SliderFloat("theta", &_theta, 0.0f, glm::pi<float>());
+            }
+        }
+        ImGui::End();
+    }
+
+    void draw() override
+    {
+        //Получаем текущие размеры экрана и выставлям вьюпорт
+        int width, height;
+        glfwGetFramebufferSize(_window, &width, &height);
+
+        glViewport(0, 0, width, height);
+
+        //Очищаем буферы цвета и глубины от результатов рендеринга предыдущего кадра
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        //Подключаем шейдер		
+        _shader->use();
+
+        //Загружаем на видеокарту значения юниформ-переменных
+        _shader->setMat4Uniform("viewMatrix", _camera.viewMatrix);
+        _shader->setMat4Uniform("projectionMatrix", _camera.projMatrix);
+
+        _light.position = glm::vec3(glm::cos(_phi) * glm::cos(_theta), glm::sin(_phi) * glm::cos(_theta), glm::sin(_theta)) * (float)_lr;
+        glm::vec3 lightPosCamSpace = glm::vec3(_camera.viewMatrix * glm::vec4(_light.position, 1.0));
+
+        _shader->setVec3Uniform("light.pos", lightPosCamSpace); //копируем положение уже в системе виртуальной камеры
+        _shader->setVec3Uniform("light.La", _light.ambient);
+        _shader->setVec3Uniform("light.Ld", _light.diffuse);
+        _shader->setVec3Uniform("light.Ls", _light.specular);
+
+        glActiveTexture(GL_TEXTURE0);  //текстурный юнит 0        
+        glBindSampler(0, _sampler);
+        _worldTexture->bind();
+        _shader->setIntUniform("diffuseTex", 0);
+
+        glActiveTexture(GL_TEXTURE1);  //текстурный юнит 1        
+        glBindSampler(1, _sampler);
+        _lookupTexture->bind();
+        _shader->setIntUniform("lookupTex", 1);
+
+        //Загружаем на видеокарту матрицы модели мешей и запускаем отрисовку
+        {
+            _shader->setMat4Uniform("modelMatrix", _cube->modelMatrix());
+            _shader->setMat3Uniform("normalToCameraMatrix", glm::transpose(glm::inverse(glm::mat3(_camera.viewMatrix * _cube->modelMatrix()))));
+
+            _cube->draw();
+        }
+
+        {
+            _shader->setMat4Uniform("modelMatrix", _sphere->modelMatrix());
+            _shader->setMat3Uniform("normalToCameraMatrix", glm::transpose(glm::inverse(glm::mat3(_camera.viewMatrix * _sphere->modelMatrix()))));
+
+            _sphere->draw();
+        }
+
+        {
+            _shader->setMat4Uniform("modelMatrix", _bunny->modelMatrix());
+            _shader->setMat3Uniform("normalToCameraMatrix", glm::transpose(glm::inverse(glm::mat3(_camera.viewMatrix * _bunny->modelMatrix()))));
+
+            _bunny->draw();
+        }
+
+        //Рисуем маркеры для всех источников света		
+        {
+            _markerShader->use();
+
+            _markerShader->setMat4Uniform("mvpMatrix", _camera.projMatrix * _camera.viewMatrix * glm::translate(glm::mat4(1.0f), _light.position));
+            _markerShader->setVec4Uniform("color", glm::vec4(_light.diffuse, 1.0f));
+            _marker->draw();
+        }
+
+        //Отсоединяем сэмплер и шейдерную программу
+        glBindSampler(0, 0);
+        glUseProgram(0);
+    }
+};
+
+int main()
+{
+    SampleApplication app;
+    app.start();
+
+    return 0;
+}
---
+++ seminar5/shaders5/textureLookup.frag
@@ -0,0 +1,52 @@
+/**
+Использование текстуры в качестве коэффициента отражения света
+*/
+
+#version 330
+
+uniform sampler2D diffuseTex;
+uniform sampler2D lookupTex;
+
+struct LightInfo
+{
+	vec3 pos; //положение источника света в системе координат ВИРТУАЛЬНОЙ КАМЕРЫ!
+	vec3 La; //цвет и интенсивность окружающего света
+	vec3 Ld; //цвет и интенсивность диффузного света
+	vec3 Ls; //цвет и интенсивность бликового света
+};
+uniform LightInfo light;
+
+in vec3 normalCamSpace; //нормаль в системе координат камеры (интерполирована между вершинами треугольника)
+in vec4 posCamSpace; //координаты вершины в системе координат камеры (интерполированы между вершинами треугольника)
+in vec2 texCoord; //текстурные координаты (интерполирована между вершинами треугольника)
+
+out vec4 fragColor; //выходной цвет фрагмента
+
+const vec3 Ks = vec3(1.0, 1.0, 1.0); //Коэффициент бликового отражения
+const float shininess = 128.0;
+
+void main()
+{
+	vec3 diffuseColor = texture(diffuseTex, texCoord).rgb;
+
+	vec3 normal = normalize(normalCamSpace); //нормализуем нормаль после интерполяции
+	vec3 viewDirection = normalize(-posCamSpace.xyz); //направление на виртуальную камеру (она находится в точке (0.0, 0.0, 0.0))
+	
+	vec3 lightDirCamSpace = normalize(light.pos - posCamSpace.xyz); //направление на источник света	
+
+	float NdotL = max(dot(normal, lightDirCamSpace.xyz), 0.0); //скалярное произведение (косинус)
+
+	vec3 color = diffuseColor * (light.La + light.Ld * NdotL);
+
+	if (NdotL > 0.0)
+	{			
+		vec3 halfVector = normalize(lightDirCamSpace.xyz + viewDirection); //биссектриса между направлениями на камеру и на источник света
+
+        vec2 lookupCoords = vec2(max(dot(normal, halfVector), 0.0), shininess / 255.0);
+        float blinnTerm = texture(lookupTex, lookupCoords).r;
+
+		color += light.Ls * Ks * blinnTerm;
+	}
+
+	fragColor = vec4(color, 1.0);
+}
