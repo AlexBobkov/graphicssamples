1feb5cbd1344bf30fca727845d991ff9ae653f5f
Alexander Bobkov <alexander.e.bobkov@gmail.com>
2016-04-30 02:31:07 +0300

    Added tesselation sample

---
+++ seminar10/Sample_10_7_Tesselation.cpp
@@ -0,0 +1,194 @@
+#include <Application.hpp>
+#include <LightInfo.hpp>
+#include <Mesh.hpp>
+#include <ShaderProgram.hpp>
+#include <Texture.hpp>
+
+#include <glm/gtx/transform.hpp>
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <deque>
+
+/**
+Тесселяция
+*/
+class SampleApplication : public Application
+{
+public:       
+    ShaderProgramPtr _shader;
+  
+    //Переменные для управления положением одного источника света
+    float _lr;
+    float _phi;
+    float _theta;
+
+    LightInfo _light;
+
+    //------------------------------
+
+    GLuint _vbo;
+    GLuint _vao;
+
+    int outerLevel = 8;
+    int innerLevel = 8;
+
+    int maxTessLevel = 0;
+
+    bool dynamicLevel = false;
+
+    void makeScene() override
+    {
+        Application::makeScene();
+
+        //Максимальное количество текселей в текстурном буфере
+        glGetIntegerv(GL_MAX_TESS_GEN_LEVEL, &maxTessLevel);
+
+        std::cout << "Max tesselation level " << maxTessLevel << std::endl;
+
+        //------------------------------
+
+        std::vector<glm::vec3> vertices =
+        {
+            glm::vec3(-1.0f, -1.0f, 0.0f),
+            glm::vec3(1.0f, -1.0f, 0.0f),
+            glm::vec3(1.0f, 1.0f, 0.0f),
+            glm::vec3(-1.0f, 1.0f, 0.0f)
+        };
+
+        //Создаем прямоугольник
+        glGenBuffers(1, &_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float) * 3, vertices.data(), GL_STATIC_DRAW);
+        
+        glGenVertexArrays(1, &_vao);
+        glBindVertexArray(_vao);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+        glBindVertexArray(0);
+
+        //=========================================================
+        //Инициализация шейдеров
+
+        _shader = std::make_shared<ShaderProgram>();
+
+        ShaderPtr vs = std::make_shared<Shader>(GL_VERTEX_SHADER);
+        vs->createFromFile("shaders10/tess.vert");
+        _shader->attachShader(vs);
+
+        ShaderPtr tcs = std::make_shared<Shader>(GL_TESS_CONTROL_SHADER);
+        tcs->createFromFile("shaders10/tess.control");
+        _shader->attachShader(tcs);
+        
+        ShaderPtr tes = std::make_shared<Shader>(GL_TESS_EVALUATION_SHADER);
+        tes->createFromFile("shaders10/tess.eval");
+        _shader->attachShader(tes);
+        
+        ShaderPtr fs = std::make_shared<Shader>(GL_FRAGMENT_SHADER);
+        fs->createFromFile("shaders10/tess.frag");
+        _shader->attachShader(fs);
+
+        _shader->linkProgram();
+        
+        //=========================================================
+        //Инициализация значений переменных освщения
+        _lr = 10.0;
+        _phi = 0.0f;
+        _theta = 0.48f;
+
+        _light.position = glm::vec3(glm::cos(_phi) * glm::cos(_theta), glm::sin(_phi) * glm::cos(_theta), glm::sin(_theta)) * (float)_lr;
+        _light.ambient = glm::vec3(0.2, 0.2, 0.2);
+        _light.diffuse = glm::vec3(0.8, 0.8, 0.8);
+        _light.specular = glm::vec3(1.0, 1.0, 1.0);
+    }
+
+    void updateGUI() override
+    {
+        Application::updateGUI();
+
+        ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiSetCond_FirstUseEver);
+        if (ImGui::Begin("MIPT OpenGL Sample", NULL, ImGuiWindowFlags_AlwaysAutoResize))
+        {
+            ImGui::Text("FPS %.1f", ImGui::GetIO().Framerate);
+
+            if (ImGui::CollapsingHeader("Light"))
+            {
+                ImGui::ColorEdit3("ambient", glm::value_ptr(_light.ambient));
+                ImGui::ColorEdit3("diffuse", glm::value_ptr(_light.diffuse));
+                ImGui::ColorEdit3("specular", glm::value_ptr(_light.specular));
+
+                ImGui::SliderFloat("radius", &_lr, 0.1f, 10.0f);
+                ImGui::SliderFloat("phi", &_phi, 0.0f, 2.0f * glm::pi<float>());
+                ImGui::SliderFloat("theta", &_theta, 0.0f, glm::pi<float>());
+            }
+
+            ImGui::SliderInt("outer tess", &outerLevel, 1, maxTessLevel);
+            ImGui::SliderInt("inner tess", &innerLevel, 1, maxTessLevel);
+
+            ImGui::Checkbox("dynamic", &dynamicLevel);
+        }
+        ImGui::End();
+    }
+
+    void update()
+    {
+        Application::update();
+
+        _light.position = glm::vec3(glm::cos(_phi) * glm::cos(_theta), glm::sin(_phi) * glm::cos(_theta), glm::sin(_theta)) * (float)_lr;
+    }
+
+    void draw() override
+    {
+        //Получаем текущие размеры экрана и выставлям вьюпорт
+        int width, height;
+        glfwGetFramebufferSize(_window, &width, &height);
+
+        glViewport(0, 0, width, height);
+
+        //Очищаем буферы цвета и глубины от результатов рендеринга предыдущего кадра
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        drawScene(_shader);
+
+        //Отсоединяем сэмплер и шейдерную программу
+        glBindSampler(0, 0);
+        glUseProgram(0);
+    }
+
+    void drawScene(const ShaderProgramPtr& shader)
+    {
+        shader->use();
+
+        shader->setMat4Uniform("modelMatrix", glm::mat4(1.0f));
+        shader->setMat4Uniform("viewMatrix", _camera.viewMatrix);
+        shader->setMat4Uniform("projectionMatrix", _camera.projMatrix);
+
+        glm::vec3 lightPosCamSpace = glm::vec3(_camera.viewMatrix * glm::vec4(_light.position, 1.0));
+        shader->setVec3Uniform("light.pos", lightPosCamSpace); //копируем положение уже в системе виртуальной камеры
+        shader->setVec3Uniform("light.La", _light.ambient);
+        shader->setVec3Uniform("light.Ld", _light.diffuse);
+        shader->setVec3Uniform("light.Ls", _light.specular);
+
+        shader->setIntUniform("outerLevel", outerLevel);
+        shader->setIntUniform("innerLevel", innerLevel);          
+
+        shader->setIntUniform("dynamicLevel", dynamicLevel ? 1 : 0);
+
+        glPatchParameteri(GL_PATCH_VERTICES, 4);
+
+        glBindVertexArray(_vao);
+        glDrawArrays(GL_PATCHES, 0, 4);        
+    }
+};
+
+int main()
+{
+    SampleApplication app;
+    app.start();
+
+    return 0;
+}
---
+++ seminar10/shaders10/tess.frag
@@ -0,0 +1,45 @@
+#version 430 core
+
+struct LightInfo
+{
+	vec3 pos; //положение источника света в мировой системе координат (для точечного источника)
+	vec3 La; //цвет и интенсивность окружающего света
+	vec3 Ld; //цвет и интенсивность диффузного света
+	vec3 Ls; //цвет и интенсивность бликового света
+};
+uniform LightInfo light;
+
+in vec2 texCoords;
+in vec3 normalCamSpace;
+in vec3 posCamSpace;
+
+out vec4 fragColor; //выходной цвет фрагмента
+
+const vec3 Ka = vec3(1.0, 1.0, 1.0);
+const vec3 Kd = vec3(1.0, 1.0, 1.0);
+const vec3 Ks = vec3(1.0, 1.0, 1.0);
+const float shininess = 128.0;
+
+void main()
+{
+    vec3 normal = normalize(normalCamSpace); //нормализуем нормаль после интерполяции
+	vec3 viewDirection = normalize(-posCamSpace.xyz); //направление на виртуальную камеру (она находится в точке (0.0, 0.0, 0.0))
+
+    vec3 lightDirCamSpace = normalize(light.pos - posCamSpace); //направление на источник света	
+
+    float NdotL = max(dot(normal, lightDirCamSpace), 0.0); //скалярное произведение (косинус)
+
+    vec3 color = light.La * Ka + light.Ld * Kd * NdotL; //цвет вершины
+
+    if (NdotL > 0.0)
+    {			
+        vec3 halfVector = normalize(lightDirCamSpace + viewDirection); //биссектриса между направлениями на камеру и на источник света
+
+        float blinnTerm = max(dot(normal, halfVector), 0.0); //интенсивность бликового освещения по Блинну				
+        blinnTerm = pow(blinnTerm, shininess); //регулируем размер блика
+
+        color += light.Ls * Ks * blinnTerm;
+    }	
+
+	fragColor = vec4(color, 1.0);
+}
