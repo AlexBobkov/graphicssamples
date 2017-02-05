d3d6b8bbd033cc90cf7a7066210e9cc3e670c60b
Alexander Bobkov <alexander.e.bobkov@gmail.com>
2017-02-05 17:49:44 +0300

    Linux fixes

--- cmake/Findassimp.cmake
+++ cmake/Findassimp.cmake
@@ -53,24 +53,35 @@ if(WIN32)
 else(WIN32)
 
 	find_path(
-	  assimp_INCLUDE_DIRS
-	  NAMES postprocess.h scene.h version.h config.h cimport.h
+	  ASSIMP_INCLUDE_DIR
+	  NAMES assimp/anim.h
 	  PATHS /usr/local/include/
 	)
 
 	find_library(
-	  assimp_LIBRARIES
+	  ASSIMP_LIBRARY_RELEASE
 	  NAMES assimp
 	  PATHS /usr/local/lib/
 	)
 
-	if (assimp_INCLUDE_DIRS AND assimp_LIBRARIES)
+	find_library(
+	  ASSIMP_LIBRARY_DEBUG
+	  NAMES assimpd
+	  PATHS /usr/local/lib/
+	)
+
+	set(ASSIMP_LIBRARY 
+		optimized 	${ASSIMP_LIBRARY_RELEASE}
+		debug		${ASSIMP_LIBRARY_DEBUG}
+	)
+
+	if (ASSIMP_INCLUDE_DIR AND ASSIMP_LIBRARY)
 	  SET(assimp_FOUND TRUE)
-	ENDIF (assimp_INCLUDE_DIRS AND assimp_LIBRARIES)
+	ENDIF (ASSIMP_INCLUDE_DIR AND ASSIMP_LIBRARY)
 
 	if (assimp_FOUND)
 	  if (NOT assimp_FIND_QUIETLY)
-		message(STATUS "Found asset importer library: ${assimp_LIBRARIES}")
+		message(STATUS "Found asset importer library: ${ASSIMP_LIBRARY}")
 	  endif (NOT assimp_FIND_QUIETLY)
 	else (assimp_FOUND)
 	  if (assimp_FIND_REQUIRED)
